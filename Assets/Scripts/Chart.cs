f48b7d02689e15fd12464e89f1066bca9de8fc2a
TylerGwynn <tylergwynn23@gmail.com>
2021-01-19 16:48:13 -0500

    Notes and chart grid scroll

--- Assets/Scripts/Chart.cs
+++ Assets/Scripts/Chart.cs
@@ -7,20 +7,28 @@ public class Chart : MonoBehaviour
     public songTimer timer;
     public songLoader songLoader;
     protected Song song ;
+    protected GameObject songTop;
+    private Material grid;
 
+    Quaternion rot = new Quaternion(0,0,0,0);
+    Vector3 chartTop;
     public Material quarterMaterial;
     public Material eighthMaterial;
     public Material sixteenthMaterial;
     // Start is called before the first frame update
     void Start()
     {
+        Vector3 po = this.transform.position;
+        chartTop = new Vector3(po.x, po.y + this.transform.localScale.y /2);
+        songTop = Instantiate(new GameObject(), chartTop, rot);
+        grid = this.GetComponent<Renderer>().material;
         //TODO make songPlayer superclass?
         if (timer == null){
             timer = (songTimer)GameObject.Find("songTimer").GetComponent(typeof(songTimer));
         }
         if (songLoader == null){
             print("No song detected for "+this.gameObject.name+", using placeholder song.");
-            song = new Song("sixteenth","1   1 111 1111 11 ");
+            song = new Song("eighth","1 1 111 1111 11 ");
         }else{
             song = songLoader.song;
         }
@@ -37,22 +45,25 @@ public class Chart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //TODO BPM math
+        scrollTex(Time.deltaTime);
+        scrollNotes(Time.deltaTime);
     }
 
     void spawnNotes(){
         Vector3 notePos;
         float offset;
-        Quaternion rot = new Quaternion(0,0,0,0);
+        float toChartTop = this.transform.localScale.y /2;
+        
         GameObject curr;
         foreach (Note n in song.notes){
-            offset = -(n.tick*0.25f);
+            offset = -(n.tick*(toChartTop/16));
             // TODO can change x pos, and add types here
-            notePos = new Vector3(0, offset+2, -1) + this.transform.position;
+            notePos = new Vector3(0, offset + toChartTop, -1) + this.transform.position;
             print("tick: "+n.tick);
             // TODO optimize by only creating this bar + next... put in Update()...
             curr = Instantiate(GameObject.Find("cone"), notePos, rot);
-            curr.transform.parent = this.transform;
+            curr.transform.parent = songTop.transform;
             if (n.isQuarter()){
                 curr.GetComponent<Renderer>().material = quarterMaterial;
             }else if(n.isEighth()){
@@ -61,6 +72,14 @@ public class Chart : MonoBehaviour
                 curr.GetComponent<Renderer>().material = sixteenthMaterial;
             }
         }
+    }
+
+    void scrollTex(float y){
+        Vector2 currOffset = grid.GetTextureOffset("_MainTex");
+        grid.SetTextureOffset("_MainTex",currOffset + new Vector2(0,y));
+    }
+    void scrollNotes(float y){
+        songTop.transform.Translate(0,-y*this.transform.localScale.y/2,0);
     }
 
 }
