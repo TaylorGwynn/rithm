9920039f4b59afbd89ef165676925c3f4763613a
TylerGwynn <tylergwynn23@gmail.com>
2021-01-19 01:06:16 -0500

    song constructors take length as 16ths or string

--- Assets/Scripts/jsonTest.cs
+++ Assets/Scripts/jsonTest.cs
@@ -33,53 +33,48 @@ public class Note{
 
 public class Song{
     public enum BeatSize{
-        whole = 1,
-        half = 2,
-        quarter = 3,
-        eighth = 8,
-        sixteenth = 16
+        whole = 16,
+        half = 8,
+        quarter = 4,
+        eighth = 2,
+        sixteenth = 1
     }
     public string name {get; set;}
     public List<Note> notes {get; set;}
-
-
-    // creates a song with a list of notes with any characters/spaces, indicating either
-    //  "whole", "half", "quarter", "eighth" or "sixteenth" notes/rests respectively.
-    // public Song(string fromType, string fromBeats){
-    //     BeatSize size = (BeatSize)Enum.Parse(typeof(BeatSize),fromType,true);
-    //     Song(size, fromBeats);
-        
-    // }
 
     // creates a song with a list of notes with any characters/spaces, indicating either
     //  "whole", "half", "quarter", "eighth" or "sixteenth" notes/rests respectively.
     public Song(string fromType, string fromBeats){
         
-        string t = fromType.ToLower();
-        int beatSize; //number to "multiply" to match the number of 16ths each char represents
-        if       (t    == "whole" || t == "1"){
-            beatSize = 16;
-        }else if (t    == "half" || t == "2"){
-            beatSize = 8;
-        }else if (t    == "quarter" || t == "4"){
-            beatSize = 4;
-        }else if (t    == "eighth" || t == "8"){
-            beatSize = 2;
-        }else if (t    == "sixteenth" || t == "16"){
-            beatSize = 1;
-        } else{
-            Debug.Log("Unknown Song(fromType) '"+fromType+"', using 16ths.");
-            beatSize = 1;
+        BeatSize bSize = BeatSize.sixteenth;
+        try{
+        bSize = (BeatSize)Enum.Parse(typeof(BeatSize),fromType,true);
+        }catch(ArgumentException){
+        Debug.Log("Unknown type in Song(fromType) '"+fromType+"', using 16ths.");
         }
 
-        int index = 0;
+        long index = 0;
         foreach (char c in fromBeats)
         {
             // TODO change "any character" to match different types of notes
             if(c != ' '){
                 this.notes.Add(new Note(index));
             }
-            index += beatSize;
+            index += (long)bSize;
+        }
+    }
+    // creates a song with a list of notes with any characters/spaces, indicating their length
+    // as a multiple of 16th notes/rests respectively.
+    public Song(long beatLenSixteenths, string fromBeats){
+        
+        long index = 0;
+        foreach (char c in fromBeats)
+        {
+            // TODO change "any character" to match different types of notes
+            if(c != ' '){
+                this.notes.Add(new Note(index));
+            }
+            index += beatLenSixteenths;
         }
     }
 }
